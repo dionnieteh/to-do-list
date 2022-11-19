c1ce2f2c90a52c5cbc553a2e56433455eab9851f
dionnieteh <107110983+dionnieteh@users.noreply.github.com>
2022-11-20 02:46:25 +0800

    final commit

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -22,55 +21,57 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
-    // we need to stored the to do list items in the array list
-    // because we dont know how many task list item the user can add
-    // its better we used arraylist rather than array
-    // we used an array adapter here as a middlemen to connect the arraylist
-    // with the listview
-    // arraylist --> array adapter --> listview
+    /*
+    1. store to do list items in the array list rather than array
+    because we dont know how many task list item the user will add
+    2. use array adapter as a middlemen to connect the arraylist with listview
+    (arraylist --> array adapter --> listview)
+    */
     static ArrayList<String> notes = new ArrayList<>();
     static ArrayAdapter arrayAdapter;
-    // create a menu bar for the apps
 
+    // when user clicks on ImageButton
+    public void addNew(View view){
+        Intent intent = new Intent(getApplicationContext(),
+                note_editor.class); // open and link and page
+        startActivity(intent);
+    }
+
+    // create menu bar for the app
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater(); // pop up menu
         menuInflater.inflate(R.menu.main_menu, menu); // get menu from xml
         return super.onCreateOptionsMenu(menu); // return menu object
     }
 
-    // what happen if the user select the menu item - add button
-    // when the user select the menu item add in the menu bar
-    // create an intent object, we used an explicit intent here
-    // bring the user to another page or activity
+    /*
+    when user select add note at menu,
+    create an explicit intent object to
+    bring the user to another page or activity
+    */
     public boolean onOptionsItemSelected(MenuItem item) {
         super.onOptionsItemSelected(item);
-        if (item.getItemId() == R.id.add_note) {
+        if (item.getItemId() == R.id.add_note) { // if user chooses add note
             Intent intent = new Intent(getApplicationContext(),
-                    note_editor.class); // open the link and page
+                    note_editor.class); // open and link to note_editor class
             startActivity(intent);
-            return true; // the user select add menu item
+            return true; // the user selects add menu item
         }
-        else if (item.getItemId() == R.id.about_us){
+        else if (item.getItemId() == R.id.about_us){ // if user chooses about us
             Intent intent = new Intent(getApplicationContext(),
-                    about_us.class); // open the link and page
+                    about_us.class); // open and link to about_us page
             startActivity(intent);
-            return true;
+            return true; // the user selects about us item
         }
-        //diy about us here
-
         return false; // the user did not select the menu item
-    }
-
-    public void addNew(View view){
-        Intent intent = new Intent(getApplicationContext(),
-                note_editor.class); // open the link and page
-        startActivity(intent);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // optional add note ImageButton
         ImageButton addBtn = (ImageButton) findViewById(R.id.add);
 
         // get the list view object from xml files
@@ -78,16 +79,16 @@ public class MainActivity extends AppCompatActivity {
 
        // setup the shared preferences object in this app
        // for private use only, the SP aka file can be accessed by this app only
-        SharedPreferences sharedPreferences =
-                getApplicationContext().getSharedPreferences
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences
                         ("com.example.assignment2", Context.MODE_PRIVATE);
 
-        // the shared preferences object cannot work directly with the arraylist
-        // this app will stored all the user input into array list first
-        // then convert the arraylist into hash set before we can save
-        // the user input into SP
-        // user input --> array list --> hash set --> shared preferences
-        // array list cannot --> shared preferences, need hash set as middlemen
+        /*
+        1. shared preferences object cannot work directly with the arraylist
+        2. this app will stored all the user input into array list first
+        then convert the arraylist into hash set before we can save the user input into SP
+        3. user input --> array list --> hash set --> shared preferences
+        4. array list cannot --> shared preferences, need hash set as middlemen
+        */
         HashSet<String> set = (HashSet<String>)
                 sharedPreferences.getStringSet("notes", null);
         //Log.i("test", set.toString());
@@ -126,39 +127,39 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemLongClickListener
                 (new AdapterView.OnItemLongClickListener() {
 
-                    public boolean onItemLongClick
-                            (AdapterView<?> adapterView, View view, int i, long l) {
-                        final int itemToDelete = i; // which item the user wish to delete
-                        // create a new alert dialog box
-                        // if the user click yes button
-                        // if the the user click no button
-                        new AlertDialog.Builder(MainActivity.this)
-                                .setIcon(R.drawable.popup)
-                                .setTitle("Are you sure?")
-                                .setMessage("Do you want to delete this note?")
-                                .setPositiveButton("Yes",
-                                        new DialogInterface.OnClickListener() {
-                                            public void onClick(DialogInterface dialogInterface, int i) {
-                                                notes.remove(itemToDelete);
-                                                // remove items on the notes array list
-                                                arrayAdapter.notifyDataSetChanged();
-                                                // update the listview via array adapter
-                                                SharedPreferences sharedPreferences =
-                                                        getApplicationContext().getSharedPreferences
-                                                                ("com.example.assignment2",
-                                                                        Context.MODE_PRIVATE); // open SP or file
-                                                HashSet<String> set = new HashSet(MainActivity.notes);
-                                                // convert array list to hash set
-                                                sharedPreferences.edit().putStringSet("notes", set).apply();
-                                                // open the SP, put new data into SP, save all changes
-                                                // you can edit or delete one item at one time only
-                                            }
-                                        }
-                                )
-                                .setNegativeButton("No", null)
-                                .show();
-                        return true;
-                    }
-                });
+            public boolean onItemLongClick
+                    (AdapterView<?> adapterView, View view, int i, long l) {
+                final int itemToDelete = i; // which item the user wish to delete
+                // create a new alert dialog box
+                // if the user click yes button
+                // if the the user click no button
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(R.drawable.popup)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes",
+                                new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                notes.remove(itemToDelete);
+                                // remove items on the notes array list
+                                arrayAdapter.notifyDataSetChanged();
+                                // update the listview via array adapter
+                                SharedPreferences sharedPreferences =
+                                        getApplicationContext().getSharedPreferences
+                                                ("com.example.assignment2",
+                                                        Context.MODE_PRIVATE); // open SP or file
+                                HashSet<String> set = new HashSet(MainActivity.notes);
+                                // convert array list to hash set
+                                sharedPreferences.edit().putStringSet("notes", set).apply();
+                                // open the SP, put new data into SP, save all changes
+                                // you can edit or delete one item at one time only
+                            }
+                        }
+                        )
+                        .setNegativeButton("No", null)
+                        .show();
+                return true;
+            }});
     }
 }
